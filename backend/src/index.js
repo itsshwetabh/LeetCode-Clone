5217fcdac1ce2d64d51c938b810cc6632951eafc
Shwetabh <shwetabhbhu@gmail.com>
2025-07-27 23:03:17 +0530

    cors origin set

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const cors = require('cors')
 
 app.use(cors({
     // origin: 'http://localhost:5173',
-    origin: ['https://leet-code-clone-nu.vercel.app/', 'http://localhost:5173'],
+    origin: ['https://leet-code-clone-nu.vercel.app', 'http://localhost:5173'],
     credentials: true 
 }))
 
